b670da619096f997d592a887e760af269ba92e7c
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-07-16 15:02:54 +0500

    console-functions

--- js/js_02_script.js
+++ js/js_02_script.js
@@ -1,1 +1,37 @@
-console.log("external msg")
+// (step1) To see runtime of your code
+console.time("your code took");
+
+// can print anything
+console.log("external msg");
+
+// can use it as a calculator
+console.log(67*9);
+
+// can use for different data types
+console.log(true);
+
+//for array
+console.log([1000,384,48,44977]);
+
+//for object
+console.log({daniyal:"this", minhal:5});
+
+//for table
+console.table({brook:"this", big:5});
+
+//to throw error
+// console.warn("this is a warning");
+
+//to clear
+// console.clear();
+
+// (step2) To see runtime of your code
+console.timeEnd("your code took");
+
+// To check condition and then throw error
+console.assert(566<189 , "age should be less than 189");
+
+//error
+console.error("This is an error");
+
+
